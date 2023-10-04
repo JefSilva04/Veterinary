38391d44c1da143645a18e79ceb1fd6550a9560c
JEFERSON STEVEN SILVA AGUIRRE <jefersonsilva262228@correo.itm.edu.co>
2023-10-04 14:16:59 -0500

    Visualizacion de Owners en la web desde el API

---
+++ Veterinary.API/Controllers/OwnersController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Veterinary.API.Data;
+using Veterinary.Shared.Entities;
+
+namespace Veterinary.API.Controllers
+{
+    [ApiController]
+    [Route("api/owners")]
+    public class OwnersController:ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public OwnersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Get con lista
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            //200
+            return Ok(await _context.Owners.ToListAsync());
+        }
+
+        //Get por parametro
+        [HttpGet("{Id:int}")]
+        public async Task<ActionResult> Get(int Id)
+        {
+            //200
+            var owner = await _context.Owners.FirstOrDefaultAsync(x => x.Id == Id);
+            
+            if (owner == null)
+            {
+                return NotFound();
+            }
+            return Ok(owner);
+        }
+
+        //Insertar un registro
+        [HttpPost]
+        public async Task<ActionResult> Post(Owner owner)
+        {
+            _context.Add(owner);
+            await _context.SaveChangesAsync();
+            return Ok(owner);
+        }
+    }
+}
